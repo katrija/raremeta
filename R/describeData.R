b3d487d07b6e09e21f9ddef8143fba9821d48782
katrija <katrinjansen@uni-muenster.de>
2022-04-05 16:41:58 +0200

    Add function which describes a meta-analytic data set (e.g., number of double-zero studies, number of single-zero studies).

---
+++ R/describeData.R
@@ -0,0 +1,29 @@
+describeData <- function(data, ai = NULL, bi = NULL, ci = NULL, di = NULL, n1i = NULL, n2i = NULL){
+
+  mf <- match.call()
+
+  mf.ai <- mf[[match("ai", names(mf))]]
+  mf.bi <- mf[[match("bi", names(mf))]]
+  mf.ci <- mf[[match("ci", names(mf))]]
+  mf.di <- mf[[match("di", names(mf))]]
+  mf.n1i <- mf[[match("n1i", names(mf))]]
+  mf.n2i <- mf[[match("n2i", names(mf))]]
+
+  ai <- eval(mf.ai, data)
+  bi <- eval(mf.bi, data)
+  ci <- eval(mf.ci, data)
+  di <- eval(mf.di, data)
+  n1i <- eval(mf.n1i, data)
+  n2i <- eval(mf.n2i, data)
+
+  if(!is.data.frame(data)){
+    stop("First argument must be a data frame.")
+  }
+
+  nEventsT <- sum(ai, na.rm = TRUE)
+  nEventsC <- sum(ci, na.rm = TRUE)
+
+  descEvents <- paste("There are", nEventsT, "events in the treatment group and", nEventsC, "in the control group.")
+
+  return(descEvents)
+}
