5a151c8764cd34e53af0c2a1eaa78598ad9bd4a4
katrija <katrinjansen@uni-muenster.de>
2022-10-31 10:33:01 +0100

    Add heterogeneity information.

--- R/print.raremeta.R
+++ R/print.raremeta.R
@@ -34,11 +34,30 @@ print.raremeta <- function(x, digits, ...){
     cat(paste0("\nContinuity correction: ", cc, ", applied to ", sum(x$cc.studies), " studies"))
     cat("\nDouble-zero studies were", drop00, "the analysis.", "\n")
 
+    # Heterogeneity:
+    qpval <- ifelse(x$QEp < .001, "< .001", round(x$QEp, 3))
+
+    cat("\nHeterogeneity: \n")
+    cat("\n", paste0("Q(df = ", x$k-1, ") = ", round(x$QE, digits), ", p-val ", qpval), "\n")
+
+    if(is.element(x$method, c("CE", "EE", "FE"))){
+      cat("\n",
+          "I^2 (total heterogeneity / total variability): ", round(x$I2, 2), " %", "\n"
+      )
+    }else{
+      cat("\n",
+          "tau^2 (estimated amount of total heterogeneity):", round(x$tau2, digits), "\n",
+          "tau (square root of estimated tau^2 value):     ", round(sqrt(x$tau2), digits), "\n",
+          "I^2 (total heterogeneity / total variability):  ", round(x$I2, 2), " %", "\n"
+      )
+    }
+
+    # Model results:
     signif <- stats::symnum(x$pval, corr=FALSE, na=FALSE,
                             cutpoints=c(0, 0.001, 0.01, 0.05, 0.1, 1), symbols = c("***", "**", "*", ".", " "))
 
     res.table <- data.frame(round(as.numeric(x$beta), digits), round(x$se, digits),
-                            round(x$zval, digits), ifelse(x$pval > 0.001, round(x$pval, 3), "< .001"),
+                            round(x$zval, digits), ifelse(x$pval < 0.001, "< .001", round(x$pval, 3)),
                             round(x$ci.lb, digits), round(x$ci.ub, digits))
     names(res.table) <- c(as.character(x$measure), "se", "zval", "pval",  "ci.lb", "ci.ub")
 
@@ -52,7 +71,7 @@ print.raremeta <- function(x, digits, ...){
     cat("\n")
 
     cat("---")
-    cat("\nSignif. codes: ", "0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1" )
+    cat("\nSignif. codes: ", "'***': < .001 '**': < .01 '*': < .05 '.': < .1" )
 
 
   }
