c967ab2e5100250498eb0991ed530ba8a71bb026
katrija <katrinjansen@uni-muenster.de>
2022-10-31 11:47:21 +0100

    Make Nissen and Wolski (2007) data publicly available.

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Meta-analytic data from Nissen and Wolski (2007)
+#'
+#' @docType data
+#'
+#' @usage dat.nissen2007
+#'
+#' @format A data frame with 42 rows and 8 columns.
+#'
+#' @references Nissen and Wolski (2007) (\href{https://www.nejm.org/doi/full/10.1056/NEJMoa072761}{NEJM})
+"dat.nissen2007"
