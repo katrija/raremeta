74907e7424201667530ede2801050a523b2ff078
katrija <katrinjansen@uni-muenster.de>
2023-02-23 15:05:20 +0100

    Add basic documentation.

--- R/rareGLMM.R
+++ R/rareGLMM.R
@@ -1,22 +1,52 @@
-#' Conduct a meta-analysis using a generalized linear mixed model (GLMM)
+#' Conduct a meta-analysis using a generalized linear (mixed) model (GLMM)
 #'
-#' @param x
-#' @param measure
-#' @param intercept
-#' @param slope
-#' @param conditional
-#' @param drop00
-#' @param level
-#' @param test
-#' @param digits
-#' @param verbose
-#' @param control
-#' @param ...
-#'
-#' @return
+#' @param x an object of class `"rareData"`.
+#' @param measure character string specifying the effect size or outcome measure to be used
+#' (either `"logOR"` for the log odds ratio or `"logRR"` for the log relative risk).
+#' @param intercept character string specifying whether to fit a model with fixed, study-specific
+#' intercepts (`"fixed"`) or a random intercept (`"random"`).
+#' @param slope character string specifying whether to fit a model with a fixed slope (`"fixed"`) or a
+#' random slope (`"random"`). A model with a fixed slope is a model with homogeneous effects, that is, a
+#' fixed-effects model, while a model with a random slope is a model with heterogeneous effects, that is, a
+#' random effects model.
+#' @param conditional logical specifying whether to estimate a conditional generalized linear mixed model.
+#' Default is `FALSE`. Conditional models are not implemented yet and specifying `conditional = TRUE` will
+#' yield an error.
+#' @param drop00 logical indicating whether double-zero studies (i.e., studies with no events or
+#' only events in both groups) should be excluded when calculating the outcome measufit for the
+#' individual studies.
+#' @param level numeric between 0 and 100 specifying the confidence interval level (the default is 95).
+#' @param test character string specifying how test statistics and confidence intervals for the
+#' fixed effects should be computed (currently, only `"z"`, for Wald-type tests is available).
+#' @param digits integer specifying the number of decimal places to which the printed results
+#' should be rounded (if unspecified, the default is 4).
+#' @param verbose logical indicating whether output should be generated on the progress of model
+#' fitting (the default is `FALSE`). Can also be an integer. Values > 1 generate more verbose output.
+#' @param control optional list of control values for the iterative algorithms. If unspecified, default
+#' values are defined inside the functions.
+#' @param ... additional arguments.
+#' @return an object of class "raremeta". The object is a list containing the following elements:
+#' * `model`: name of the model used for conducting the meta-analysis.
+#' * `beta`: estimated coefficients of the model.
+#' * `se`: standard errors of the  coefficients.
+#' * `zval`: test statistics of the coefficients.
+#' * `zval`: p-values corresponding to the test statistics.
+#' * `ci.lb`: lower bound of the confidence intervals for the coefficients.
+#' * `ci.ub`: upper bound of the confidence intervals for the coefficients.
+#' * `vb`: variance-covariance matrix of the estimated coefficients.
+#' * `tau2`: estimated amount of (residual) heterogeneity. Always `0` when `method = "FE"`.
+#' * `fit.stats`: a list with log-likelihood, deviance, AIC, BIC, and AICc values under
+#' the unrestricted and restricted likelihood.
+#' * `p`: number of coefficients in the model (including the intercept).
+#' * `k`: number of studies included in the analysis.
+#' * `k.all`: total number of studies (before exclusion).
+#' * `kdz`,`ksz`: number of double-zero and single-zero studies.
+#' * `k1sz`, `k2sz`: number of single-zero studies where the zero is in group 1 or group 2.
+#' * `ai`, `bi`, `ci`, `di`: original entries of the 2x2 tables for all studies.
+#' * `ni`, `n1i`, `n2i`: original total and group sample sizes.
+#' * ...
 #' @export
 #'
-#' @examples
 rareGLMM <- function(x, measure, intercept = "fixed", slope = "random", conditional = FALSE,
                      drop00 = FALSE,
                      level = 95,
