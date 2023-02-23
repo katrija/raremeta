49be57d42c7ac8ec50caa1aed86bc4f6a5380f25
katrija <katrinjansen@uni-muenster.de>
2023-02-23 10:54:34 +0100

    Make print function usable for output from rareMH function.

--- R/print.raremeta.R
+++ R/print.raremeta.R
@@ -72,10 +72,41 @@ print.raremeta <- function(x, digits, ...){
 
     cat("---")
     cat("\nSignif. codes: ", "'***': < .001 '**': < .01 '*': < .05 '.': < .1" )
-
-
   }
 
+
+  if(x$model == "rareMH"){
+
+    #cc <- ifelse(x$cc != "tacc", x$cc, "treatment-arm")
+    #drop00 <- ifelse(x$drop00 == TRUE, "excluded from", "included in")
+
+    cat("Fixed-effects meta-analysis using the Mantel-Haenszel method:", "\n")
+
+    cat("\nNumber of studies:", x$k, "\n")
+    #cat(paste0("\nContinuity correction: ", cc, ", applied to ", sum(x$cc.studies), " studies"))
+    #cat("\nDouble-zero studies were", drop00, "the analysis.", "\n")
+
+    # Model results:
+    signif <- stats::symnum(x$pval, corr=FALSE, na=FALSE,
+                            cutpoints=c(0, 0.001, 0.01, 0.05, 0.1, 1), symbols = c("***", "**", "*", ".", " "))
+
+    res.table <- data.frame(round(as.numeric(x$beta), digits), round(x$se, digits),
+                            round(x$zval, digits), ifelse(x$pval < 0.001, "< .001", round(x$pval, 3)),
+                            round(x$ci.lb, digits), round(x$ci.ub, digits))
+    names(res.table) <- c(as.character(x$measure), "se", "zval", "pval",  "ci.lb", "ci.ub")
+
+
+    res.table <- cbind(res.table, signif)
+    colnames(res.table)[ncol(res.table)] <- ""
+
+    cat("\nModel results: \n")
+    cat("\n")
+    print(res.table, row.names = FALSE)
+    cat("\n")
+
+    cat("---")
+    cat("\nSignif. codes: ", "'***': < .001 '**': < .01 '*': < .05 '.': < .1" )
+  }
 
 
 }
