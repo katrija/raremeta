50eddce1dca6bd47fdb0eca05ae3d353edd0d0f0
kkwesti <98816662+kkwesti@users.noreply.github.com>
2023-06-06 17:17:15 +0200

    Starting a file where we test the functions on real data

---
+++ R/testsMetadat.R
@@ -0,0 +1,14 @@
+# testing using datasets from metadat
+library("metadat")
+
+# load packages
+data(dat.anand1999,
+     dat.axfors2021,
+     dat.cannon2006,
+     dat.collins1985a,
+     dat.collins1985b,
+     dat.damico2009,
+     dat.colditz1994,
+     dat.hartmannboyce2018,
+     dat.yusuf1985)
+
