e07c66cc6f3c5fe49ed053381bd3499e6205b671
katrija <katrinjansen@uni-muenster.de>
2023-07-21 14:19:43 +0200

    Improve handling of missing values.

--- R/rareCC.R
+++ R/rareCC.R
@@ -430,7 +430,7 @@ rareCC <- function(x, ai, bi, ci, di, n1i, n2i, data,
   ci.cc <- ci+ccc
   di.cc <- di+ccc
 
-  if(any(ai.cc == 0 | bi.cc == 0 | ci.cc == 0 |di.cc == 0)){
+  if(any(ai.cc == 0 | bi.cc == 0 | ci.cc == 0 |di.cc == 0, na.rm = TRUE)){
     stop("There can not be studies with no event in either of the cells after the application of the continuity correction. \n
       See '?rareCC()' for more options.")
   }
